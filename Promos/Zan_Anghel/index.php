7fee00d8222dcc60128b0400a3dc3642f98e8479
Ricardo Enriquez <renriquez@empsii.com>
2016-09-25 06:28:41 -0500

    Login y Establecimiento

---
+++ Promos/Zan_Anghel/index.php
@@ -0,0 +1,69 @@
+<html><style>
+.starter_container {
+	margin-top: -21px;
+	padding: 0;
+	min-width: 100%;
+	height: 100%;
+	background: url('steak.jpg') no-repeat center center;
+	background-size: cover;
+	text-align: center;
+	background-position: center;
+}
+html, body, #header {
+    margin: 0 !important;
+    padding: 0 !important;
+}
+.follow_container {
+	width: 100%;
+	position: absolute;
+	top: 35%;
+	text-align: center;
+}
+
+.col-md-offset-3 {
+	margin-left: 25%;
+}
+
+.col-md-6 {
+	width: 50%;
+}
+
+.top-title {
+	color: white;
+	font-family: 'Playball', cursive;
+	font-size: 120px;
+	font-weight: 400;
+}
+
+.white {
+	color: white;
+}
+
+.second-title {
+	font-weight: lighter;
+	font-size: 30px;
+	font-style: italic;
+	text-transform: capitalize;
+}
+
+h1, h2, p, span, a {
+	font-family: "museo-sans", helvetica, sans-serif;
+	color: #2f2e2f;
+	margin: 0;
+	padding: 0;
+}
+</style>
+</head>
+
+<body>
+	<div id="top" class="starter_container bg">
+		<div class="follow_container">
+			<div class="col-md-6 col-md-offset-3">
+				<h2 class="top-title">Restaurant</h2>
+				<h2 class="white second-title">" Best in the city "</h2>
+				<hr>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
---
+++ Promos/src/includes/iniciarSesion.php
@@ -0,0 +1,3 @@
+<?php
+
+ header('Location: ../../Zan_Anghel');
